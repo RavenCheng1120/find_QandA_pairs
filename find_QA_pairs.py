ec46b625751522e05b49f21e74dc04e73bc81802
Yun-Ting Cheng <ravencheng66@gmail.com>
2019-01-16 23:18:52 +0800

    Update find_QA_pairs.py

--- find_QA_pairs.py
+++ find_QA_pairs.py
@@ -6,18 +6,32 @@ subtitles = []
 subs = pysrt.open('ReadyPlayerOne.srt')
 for lines in subs:
 	line = lines.text.split()
+	line[0] = line[0].replace('-','')
 	subtitles.append(line[0])
 	if len(line)==2:
+		line[1] = line[1].replace('-','')
 		subtitles.append(line[1])
 #print(subtitles[0])
 
-#讀取文字檔案
+
+#讀取word2vec產生的文字檔，裡面有各種可能出現在回答中的詞
 posibleAnswers = []
 with open('AnswerWords.txt', 'r') as f:
 	for line in f:
 		line = line.replace('\n','')
 		posibleAnswers.append(line)
-print(posibleAnswers)
+#print(posibleAnswers[27])
+
+
 
 question = []
 answer = []
+for num in range(len(posibleAnswers)):
+	for index in range(len(subtitles)):
+		if posibleAnswers[num] in subtitles[index]:
+			question.append(subtitles[index-1])
+			answer.append(subtitles[index])
+			print(subtitles[index-1])
+			print(subtitles[index])
+			print('----')
+
